0e3d0f96d9703a308040c267d819ad180d6b381e
BoiLMa <30570@ma-web.nl>
2023-04-16 14:34:05 +0200

    Added functionality to some of the sensors

--- pinball/pinball.ino
+++ pinball/pinball.ino
@@ -10,39 +10,46 @@ bool addPoints = false;
 bool removeBalls = false;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   // put your setup code here, to run once:
-  score += 1;
-  Serial.println(score);
+  score += 123;
+  //Serial.println(score);
   pinMode(2, INPUT_PULLUP);   //linker knop 5
-  pinMode(3, INPUT_PULLUP);   //rechter knop 5
+  digitalWrite(2, LOW);
+  pinMode(3, INPUT_PULLUP);
+  digitalWrite(3, LOW);   //rechter knop 5
   pinMode(4, INPUT);  //linker flipper 1
   pinMode(5, INPUT);  //rechter flipper 1
-  pinMode(6, INPUT);  //linker bumper 2
+  pinMode(6, OUTPUT);  //linker bumper 2
   pinMode(7, INPUT);  //rechter bumper 2
   pinMode(8, INPUT);  //top bumper 4
   pinMode(9, INPUT);  //hole 4
 }
-
 void loop() {
-  Serial.println(score);
   // put your main code here, to run repeatedly:
-  
+  //digitalWrite(2, LOW);
   int k1 = digitalRead(2);
   int k2 = digitalRead(3);
-
-  if((k1 || k2) > 0){
-    digitalRead(2);
-    Serial.println("&");
-    digitalRead(3);
-    //score++;
-  }
-  else if((k1 || k2) <= 0)
-
   Lbumper = digitalRead(6);
   Rbumper = digitalRead(7);
   Tbumper = digitalRead(8);
-  hole = digitalRead(9);
+  holeInp = digitalRead(9);
+  holeOutp = digitalRead(10);
+  //Serial.println("knop1 " + String(k1) + " knop2 " + String(k2));
+
+  if(k2 > 0){
+    Serial.println(Lbumper);
+    digitalWrite(6, HIGH);
+  }
+  else if(k2 <= 0)
+    digitalWrite(6, LOW);
+  if(k1 > 0){
+    Serial.println(Lbumper);
+    digitalWrite(7, HIGH);
+  }
+  else if(k1 <= 0)
+    digitalWrite(, LOW);
+
   //ballsLight = analogRead(11);
 
   if (Lbumper > 0 || Rbumper > 0) {
@@ -59,6 +66,18 @@ void loop() {
       addPoints = false;
     }
   }
+
+//check if ball fell in hole
+  if(){
+    
+  }
+
+
+
+
+
+
+  //old code
   if (hole > 0) {
     removeBalls = true;
     if (removeBalls) {
